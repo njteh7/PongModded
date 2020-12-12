9cb961c392849fffc867f92a55ea1e6b06bd0b99
njteh7 <njteh77@hotmail.com>
2020-12-12 16:35:33 -0500

    Initial commit

---
+++ Assets/scripts/ScrollScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollScript : MonoBehaviour {
+    public float speed = 0;
+	
+	
+	void Update () {
+        GetComponent<Renderer>().material.mainTextureOffset = new Vector2((Time.time * speed) % 1, 0f);
+    }
+}
